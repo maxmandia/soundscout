2f480ac582fa442f3020fe51cf4160e507e2935a
MaxMandia <mmandia511@gmail.com>
2023-09-07 09:07:28 -0400

    feat/style: added date grouping

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -5,6 +5,7 @@ import { Tracks } from '@/interfaces/prisma'
 import { PlayIcon, OpenInNewWindowIcon, StopIcon } from '@radix-ui/react-icons'
 import Image from 'next/image'
 import useSWRInfinite from 'swr/infinite'
+import moment from 'moment'
 
 interface Data {
   data: Tracks[]
@@ -76,6 +77,27 @@ function Timeline() {
     return () => observer && observer.unobserve(currentRef)
   }, [data]) // Dependency on data so it re-runs when data changes
 
+  function renderDate(
+    release_date: string,
+    release_date_next: string,
+    index: number
+  ): string | null {
+    if (index === 0) {
+      const fromNow = moment(release_date).fromNow()
+      return fromNow
+    }
+
+    // compare both dates
+    let currentTrackDate = moment(release_date).fromNow()
+    let nextTrackDate = moment(release_date_next).fromNow()
+
+    if (currentTrackDate === nextTrackDate) {
+      return null
+    } else {
+      return currentTrackDate
+    }
+  }
+
   if (isLoading) {
     return <p>loading</p>
   }
@@ -91,48 +113,63 @@ function Timeline() {
           return songs.data.map((track, trackIndex) => {
             const isLastElement =
               index === data.length - 1 && trackIndex === songs.data.length - 1
+            let date
+
+            if (trackIndex === songs.data.length - 1) {
+              date = null
+            } else {
+              date = renderDate(
+                track.release_date,
+                songs.data[trackIndex + 1].release_date,
+                trackIndex
+              )
+            }
 
             return (
-              <div
-                ref={isLastElement ? lastElementRef : null}
-                key={track.id}
-                className="mb-5 flex items-center justify-between rounded-[6px] bg-input-bg p-3"
-              >
-                <div className="flex items-center gap-3">
-                  <Image
-                    className="h-[50px] w-[50px] rounded-[4px]"
-                    width={50}
-                    height={50}
-                    src={track.track_image}
-                    alt={'track image'}
-                  />
-                  <div className="flex flex-col justify-evenly">
-                    <h3 className="text-[12px]">{track.track_name}</h3>
-                    <h3 className="text-[10px]">
-                      {track.all_artists
-                        .map((artist) => artist.name)
-                        .join(', ')}
-                    </h3>
-                    <a
-                      href={track.external_url}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="flex items-center gap-[2px] text-[10px] text-input-txt underline"
-                    >
-                      <OpenInNewWindowIcon height={10} width={10} /> Open in
-                      Spotify
-                    </a>
-                  </div>
-                </div>
+              <div key={track.id}>
+                {date && (
+                  <p className="px-2 py-[6px] text-[14px] font-light">{date}</p>
+                )}
                 <div
-                  onClick={() => playSound(track.id, track.preview_url)}
-                  className="rounded-full bg-[#1DB954] p-2"
+                  ref={isLastElement ? lastElementRef : null}
+                  className="mb-5 flex items-center justify-between rounded-[6px] bg-input-bg p-3"
                 >
-                  {playingTrackId.current === track.id ? (
-                    <StopIcon height={15} width={15} />
-                  ) : (
-                    <PlayIcon />
-                  )}
+                  <div className="flex items-center gap-3">
+                    <Image
+                      className="h-[50px] w-[50px] rounded-[4px]"
+                      width={50}
+                      height={50}
+                      src={track.track_image}
+                      alt={'track image'}
+                    />
+                    <div className="flex flex-col justify-evenly">
+                      <h3 className="text-[12px]">{track.track_name}</h3>
+                      <h3 className="text-[10px]">
+                        {track.all_artists
+                          .map((artist) => artist.name)
+                          .join(', ')}
+                      </h3>
+                      <a
+                        href={track.external_url}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="flex items-center gap-[2px] text-[10px] text-input-txt underline"
+                      >
+                        <OpenInNewWindowIcon height={10} width={10} /> Open in
+                        Spotify
+                      </a>
+                    </div>
+                  </div>
+                  <div
+                    onClick={() => playSound(track.id, track.preview_url)}
+                    className="rounded-full bg-[#1DB954] p-2"
+                  >
+                    {playingTrackId.current === track.id ? (
+                      <StopIcon height={15} width={15} />
+                    ) : (
+                      <PlayIcon />
+                    )}
+                  </div>
                 </div>
               </div>
             )
